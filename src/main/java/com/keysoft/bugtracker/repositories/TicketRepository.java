a1c90c9bbb5467faa6d8edeb4cdd378d210920a2
JackTorrence <jacktorrence420@gmx.com>
2019-05-29 19:52:43 +0100

    moore moore

--- src/main/java/com/keysoft/bugtracker/repositories/TicketRepository.java
+++ src/main/java/com/keysoft/bugtracker/repositories/TicketRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.keysoft.bugtracker.domain.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 public interface    TicketRepository extends JpaRepository<Ticket, Integer> {
 }
